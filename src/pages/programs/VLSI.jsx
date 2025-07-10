7533e913e57223b3f04bbf4ab0350758141c7d4b
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2025-07-10 20:18:38 +0530

    H23

--- src/pages/programs/VLSI.jsx
+++ src/pages/programs/VLSI.jsx
@@ -92,7 +92,7 @@ const roleLogos = {
               </button>
             </a>
             <a
-              href="/src/assets/images/DSA in JAVA Syllubus-Glowlogics.pdf"
+              href="/public/ProgramsPDF/WEB Syllubus-GLOWLOGICS .pdf"
               download
             >
               <button className="border border-black text-black px-6 py-2 rounded-lg hover:bg-white transition">
